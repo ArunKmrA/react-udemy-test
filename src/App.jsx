8c6c6fac7b701b841279e2231d132e7b272e515f
Arun <mailarunkra@gmail.com>
2025-12-26 11:07:33 +0530

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Learning with ChaiNcode</h1>
+      <p>React + Vite</p>
+    </>
+  )
+}
+
+export default App
